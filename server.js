5dc6290cdccab0ffc3878b2e0f749b538aa1a30f
Victor Mombeyarara <vmombeyarara@gmail.com>
2024-12-26 18:42:44 +1100

    26-12-24 Commit [VM.2]

--- server.js
+++ server.js
@@ -79,17 +79,3 @@ const server = app.listen(port, () => {
 });
 
 module.exports = server; // Export the server instance [makes using this in unit tests easier]
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname+'/public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const port = process.env.port || 3000;
-
-app.listen(port,()=>{
-  console.log("App listening to: "+port)
-});

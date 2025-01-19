0c2d166073018e4c4e97b66da4d389ff26dea966
Victoria Todd <victoria.e.todd.98@gmail.com>
2025-01-19 17:39:11 +0100

    Dashboard - fix missing images (#22)
    * add images for medications and include fallthrough case
    
    * add images for symptoms and include fallthrough case

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,7 +4,7 @@ $(document).ready(async function () {
     if (data.statusCode === 200) {
       const user = data.user;
       console.log("User:", user);
-  
+
       // add div to #symptoms-list
       $("#greeting-message").append(`
         <span class="card-title">
@@ -19,7 +19,7 @@ $(document).ready(async function () {
       console.error("Failed to get user data:", data);
     }
   });
-  
+
   await $.get('/medications', (data) => {
     if (data.statusCode === 200) {
       const medicationData = data.data;
@@ -30,12 +30,20 @@ $(document).ready(async function () {
 
         let image = '';
 
-        if (String(record.name).toLowerCase().includes('aspirin')) image = 'aspirin.jpg';
-        else if (String(record.name).toLowerCase().includes('ibuprofen')) image = 'ibuprofen.jpg';
-        else if (String(record.name).toLowerCase().includes('paracetamol')) image = 'paracetamol.jpg';
-        else if (String(record.name).toLowerCase().includes('allegra')) image = 'allegra.jpg';
-        else if (String(record.name).toLowerCase().includes('claritin')) image = 'claritin.jpg';
+        if (String(record.name).toLowerCase().includes('astepro')) image = 'astepro.jpg';
+        else if (String(record.name).toLowerCase().includes('alaway')) image = 'alaway.jpg';
         else if (String(record.name).toLowerCase().includes('zyrtec')) image = 'zyrtec.jpg';
+        else if (String(record.name).toLowerCase().includes('claritin')) image = 'claritin.jpg';
+        else if (String(record.name).toLowerCase().includes('allegra')) image = 'allegra.jpg';
+        else if (String(record.name).toLowerCase().includes('clarinex')) image = 'clarinex.jpg';
+        else if (String(record.name).toLowerCase().includes('nasonex')) image = 'nasonex.jpg';
+        else if (String(record.name).toLowerCase().includes('flonase')) image = 'flonase.jpg';
+        else if (String(record.name).toLowerCase().includes('zetonna')) image = 'zetonna.jpg';
+        else if (String(record.name).toLowerCase().includes('lotemax')) image = 'lotemax.jpg';
+        else if (String(record.name).toLowerCase().includes('omnipred')) image = 'omnipred.jpg';
+        else if (String(record.name).toLowerCase().includes('prelone')) image = 'prelone.jpg';
+        else if (String(record.name).toLowerCase().includes('medrol')) image = 'medrol.jpg';
+        else image = 'unknown.jpg';
 
         // add div to #symptoms-list
         $('#medications-list').append(`
@@ -43,7 +51,7 @@ $(document).ready(async function () {
             <div class="card">
               <div class="card-image">
                 <img src="assets/images/medications/${image}" alt="Card Background">
-                <span class="card-title">${ record.quantity }x ${ record.name }</span>
+                <span class="card-title">${record.quantity}x ${record.name}</span>
               </div>
             </div>
           </div>
@@ -65,26 +73,25 @@ $(document).ready(async function () {
         console.log('Record:', record);
 
         let image = '';
-        if (String(record.name).toLowerCase().includes('headache')) image = 'headache.jpg';
-        else if (String(record.name).toLowerCase().includes('cough')) image = 'cough.jpg';
-        else if (String(record.name).toLowerCase().includes('congestion')) image = 'congestion.jpg';
-        else if (String(record.name).toLowerCase().includes('hayfever')) image = 'sneezing.jpg';
-        else if (String(record.name).toLowerCase().includes('fever')) image = 'fever.jpg';
-        else if (String(record.name).toLowerCase().includes('sneezing')) image = 'sneezing.jpg';
+        if (String(record.name).toLowerCase().includes('congestion')) image = 'congestion.jpg';
         else if (String(record.name).toLowerCase().includes('watery eyes')) image = 'watery-eyes.jpg';
+        else if (String(record.name).toLowerCase().includes('itchy eyes')) image = 'itchy-eyes.jpg';
+        else if (String(record.name).toLowerCase().includes('sinus pressure')) image = 'sinus-pressure.jpg';
+        else if (String(record.name).toLowerCase().includes('sneezing')) image = 'sneezing.jpg';
+        else image = 'general-malaise.jpg';
 
         let severity = '';
         if (record.severity == 1) severity = 'Mild';
         else if (record.severity == 2) severity = 'Moderate';
         else if (record.severity == 3) severity = 'Severe';
-    
+
         // add div to #symptoms-list
         $('#symptoms-list').append(`
           <div class="col s4">
             <div class="card">
               <div class="card-image">
                 <img src="assets/images/symptoms/${image}" alt="Card Background">
-                <span class="card-title">${ severity } ${ record.name }</span>
+                <span class="card-title">${severity} ${record.name}</span>
               </div>
             </div>
           </div>

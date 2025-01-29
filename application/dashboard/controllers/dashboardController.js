4ad6c4b59f41efc1c2a489d7d007316b3f43c310
Victoria Todd <victoria.e.todd.98@gmail.com>
2025-01-29 22:32:44 +0100

    working prediction api

--- application/dashboard/controllers/dashboardController.js
+++ application/dashboard/controllers/dashboardController.js
@@ -1,11 +1,13 @@
 const Medication = require('../../diary/models/Medication');
 const Symptom = require('../../diary/models/Symptom');
+const tf = require('@tensorflow/tfjs-node');
+const path = require('path');
 
 // [VM] Added this function to return the user object so it can be used for displaying the username
 
 const getUser = async (req, res) => {
 
-  const user = req.user; 
+  const user = req.user;
   if (!req.user || !req.user.email) {
     return res.status(401).json({ statusCode: 401, message: 'Unauthorized' });
   }
@@ -57,8 +59,39 @@ const getMedications = async (req, res) => {
   };
 };
 
+async function loadModel() {
+  const handler = tf.io.fileSystem("model/tfjs_model/model.json");
+  const model = await tf.loadLayersModel(handler);
+  console.log("Model loaded successfully");
+  return model;
+}
+
+// TODO: real pollen input
+const pollenInput = [ 0.30122508,  1.4705943,   0.28796168, -1.4553582,   0.8609464,   1.45584234,
+  -1.46873242,  0.29142204,  1.47742723,  0.28215821, -1.46459973, -0.90322569,
+   0.87851482,  0.88808203,  1.43907028, -0.87274583, 0.87912694];
+
+
+function preprocessInput(input) {
+  const pollenValues = Object.values(input);
+  return tf.tensor([pollenValues]);
+}
+
+const getPredictions = async (req, res) => {
+
+  const model = await loadModel();
+  const processedInput = preprocessInput(pollenInput);
+
+  const prediction = model.predict(processedInput);
+  const predictionArray = await prediction.array();
+
+  console.log("Predicted Symptoms & Medications:", predictionArray);
+
+};
+
 module.exports = {
   getSymptoms,
   getMedications,
   getUser,
+  getPredictions,
 };

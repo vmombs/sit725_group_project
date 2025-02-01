0505706d375f5960e9b486f35e26be76e55974ec
Joshua Bertucci <joshua.bertucci@deakin.edu.au>
2025-02-01 21:14:10 +1100

    Add export history feature and integrate PDF generation

--- application/my_account/routes/myAccountRoutes.js
+++ application/my_account/routes/myAccountRoutes.js
@@ -4,4 +4,6 @@ const myAccountController = require('../controllers/myAccountController');
 
 router.get('/', myAccountController.myAccount);
 
+router.get('/export-history', myAccountController.exportHistory);
+
 module.exports = router;
